4476767822d327b6a7c6c4ed0e0880ce8b46b191
William Hendrick <william.hendrick@ipaper.com>
2020-09-21 22:07:41 -0500

    Add additional mock

--- src/app/mock-games.ts
+++ src/app/mock-games.ts
@@ -5,6 +5,7 @@ export const GAMES: Game[] = [
     { id: 'NES-DH-USA', name: 'Duck Hunt' },
     { id: 'NES-EB-USA', name: 'Excitebike' },
     { id: 'NES-GY-USA', name: 'Gyromite' },
+    { id: 'NES-AL-USA', name: 'Zelda II: The Adventure of Link' },
     { id: 'NES-EI-USA', name: 'Tetris' },
     { id: 'NES-TY-USA', name: '10-Yard Fight' },
     { id: 'NES-BA-USA', name: 'Baseball' }
