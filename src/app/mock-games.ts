e4d05865e99fe047213e920fe3b8ef749d9885de
William Hendrick <william.hendrick@gmail.com>
2020-09-22 15:49:05 -0500

    add "owned" model and visual indicator

--- src/app/mock-games.ts
+++ src/app/mock-games.ts
@@ -1,12 +1,12 @@
 import { Game } from './game';
 
 export const GAMES: Game[] = [
-    { id: 'NES-SM-USA', name: 'Super Mario Brothers' },
-    { id: 'NES-DH-USA', name: 'Duck Hunt' },
-    { id: 'NES-EB-USA', name: 'Excitebike' },
-    { id: 'NES-GY-USA', name: 'Gyromite' },
-    { id: 'NES-AL-USA', name: 'Zelda II: The Adventure of Link' },
-    { id: 'NES-EI-USA', name: 'Tetris' },
-    { id: 'NES-TY-USA', name: '10-Yard Fight' },
-    { id: 'NES-BA-USA', name: 'Baseball' }
+    { id: 'NES-SM-USA', name: 'Super Mario Brothers', owned: true },
+    { id: 'NES-DH-USA', name: 'Duck Hunt', owned: true },
+    { id: 'NES-EB-USA', name: 'Excitebike', owned: false },
+    { id: 'NES-GY-USA', name: 'Gyromite', owned: false },
+    { id: 'NES-AL-USA', name: 'Zelda II: The Adventure of Link', owned: true },
+    { id: 'NES-EI-USA', name: 'Tetris', owned: false },
+    { id: 'NES-TY-USA', name: '10-Yard Fight', owned: false },
+    { id: 'NES-BA-USA', name: 'Baseball', owned: true }
 ];
