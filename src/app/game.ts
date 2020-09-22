ec2fc0c6c95e500c90250cd2d7b0b04b5314bc31
William Hendrick <william.hendrick@gmail.com>
2020-09-22 16:32:30 -0500

    Working on detail routing

--- src/app/game.ts
+++ src/app/game.ts
@@ -1,5 +1,13 @@
 export interface Game {
     id: string;
     name: string;
+    developer?: string;
+    publisher?: string;
+    region?: string;
+    releaseDate?: Date;
+    esrb?: string;
+    players?: string;
+    upc?: string;
+    board?: string;
     owned?: boolean;
   }
--- src/app/mock-games.ts
+++ src/app/mock-games.ts
@@ -1,12 +1,28 @@
 import { Game } from './game';
 
 export const GAMES: Game[] = [
-    { id: 'NES-SM-USA', name: 'Super Mario Brothers', owned: true },
+    { 
+        id: 'NES-SM-USA', 
+        name: 'Super Mario Brothers', 
+        owned: true
+    },
     { id: 'NES-DH-USA', name: 'Duck Hunt', owned: true },
     { id: 'NES-EB-USA', name: 'Excitebike' },
     { id: 'NES-GY-USA', name: 'Gyromite', owned: false },
     { id: 'NES-AL-USA', name: 'Zelda II: The Adventure of Link', owned: true },
     { id: 'NES-EI-USA', name: 'Tetris' },
-    { id: 'NES-TY-USA', name: '10-Yard Fight', owned: false },
+    { 
+        id: 'NES-TY-USA', 
+        name: '10-Yard Fight', 
+        owned: false,
+        publisher: "Nintendo", 
+        developer: "Irem", 
+        region: "NTSC (N. America)", 
+        releaseDate: new Date() ,
+        esrb: "Everyone",
+        players: "2",
+        upc: "045496630270",
+        board: "NES-NROM-256"
+    },
     { id: 'NES-BA-USA', name: 'Baseball', owned: true }
 ];
