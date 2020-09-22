e42491316162b53b5118c7a979460b60ba22cef0
William Hendrick <william.hendrick@gmail.com>
2020-09-22 16:04:26 -0500

    more gitignore, make "owned" optional

--- src/app/game.ts
+++ src/app/game.ts
@@ -1,5 +1,5 @@
 export interface Game {
     id: string;
     name: string;
-    owned: boolean;
+    owned?: boolean;
   }
--- src/app/mock-games.ts
+++ src/app/mock-games.ts
@@ -3,10 +3,10 @@ import { Game } from './game';
 export const GAMES: Game[] = [
     { id: 'NES-SM-USA', name: 'Super Mario Brothers', owned: true },
     { id: 'NES-DH-USA', name: 'Duck Hunt', owned: true },
-    { id: 'NES-EB-USA', name: 'Excitebike', owned: false },
+    { id: 'NES-EB-USA', name: 'Excitebike' },
     { id: 'NES-GY-USA', name: 'Gyromite', owned: false },
     { id: 'NES-AL-USA', name: 'Zelda II: The Adventure of Link', owned: true },
-    { id: 'NES-EI-USA', name: 'Tetris', owned: false },
+    { id: 'NES-EI-USA', name: 'Tetris' },
     { id: 'NES-TY-USA', name: '10-Yard Fight', owned: false },
     { id: 'NES-BA-USA', name: 'Baseball', owned: true }
 ];
