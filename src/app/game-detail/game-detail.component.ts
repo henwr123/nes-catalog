24dca5b570630edf2a4b47d044ead555c9198b53
William Hendrick <william.hendrick@ipaper.com>
2020-09-22 22:30:12 -0500

    More progress on the detail

--- src/app/game-detail/game-detail.component.ts
+++ src/app/game-detail/game-detail.component.ts
@@ -10,9 +10,24 @@ export class GameDetailComponent implements OnInit {
 
   @Input() game: Game;
 
+  selectedGame: Game;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.game = { 
+      id: 'NES-TY-USA', 
+      name: '10-Yard Fight', 
+      owned: false,
+      publisher: 'Nintendo', 
+      developer: 'Irem', 
+      region: 'NTSC (N. America)', 
+      releaseDate: new Date() ,
+      esrb: 'Everyone',
+      players: '2',
+      upc: '045496630270',
+      board: 'NES-NROM-256'
+    };
   }
 
 }
