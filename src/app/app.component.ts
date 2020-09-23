ebe76ce58f764e9fd1136f2ee2a31ad57e3f373a
William Hendrick <william.hendrick@gmail.com>
2020-09-23 08:30:08 -0500

    Title change and remove the integrated Detail on the Games screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'NES Catalog';
+  title = 'Nintendo Entertainment System';
 }
